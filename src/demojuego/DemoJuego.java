a825c74e033a17af04a4c5a40240b4b870f36a29
Alejok0 <ale.ghost32@gmail.com>
2022-09-29 23:45:05 -0500

    Creo la ventana inicial

--- src/demojuego/DemoJuego.java
+++ src/demojuego/DemoJuego.java
@@ -3,17 +3,8 @@ package demojuego;
 public class DemoJuego {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-<<<<<<< HEAD
-=======
-        int a = 0;
-        System.out.println(a);
-        System.out.println("XD");
-        System.out.println("jacuna matata");
-        
-        
->>>>>>> abcf159bb446b614dffd399eb370885d3ac80322
+		VentanaPrincipal ventana = new VentanaPrincipal();
+		ventana.setVisible(true);
     }
     
 }
--- src/demojuego/ventanaPrincipal.java
+++ src/demojuego/ventanaPrincipal.java
@@ -2,6 +2,17 @@ package demojuego;
 
 import javax.swing.JFrame;
 
-public class ventanaPrincipal extends JFrame{
-	
+public class VentanaPrincipal extends JFrame {
+
+public VentanaPrincipal() {
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setLayout(null);
+	setBounds(50, 50, 800, 600);
+	iniciarComponentes();
+}
+
+public void iniciarComponentes() {
+	//nada por ahora
+}
+
 }
