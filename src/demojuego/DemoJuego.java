2bf5b9e4ea96cbe36bb83d2828e51bd24cf59a78
Alejok0 <ale.ghost32@gmail.com>
2022-09-29 23:27:31 -0500

    Limpio el main

--- src/demojuego/DemoJuego.java
+++ src/demojuego/DemoJuego.java
@@ -5,11 +5,6 @@ public class DemoJuego {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int a = 0;
-        System.out.println(a);
-        
-        
-        
     }
     
 }
