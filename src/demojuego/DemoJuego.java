bc49d1481eeaf3e755bad155608b4a4a67e0f402
GDnickJB29 <jamesblane29@gmail.com>
2022-09-29 23:19:02 -0500

    Un sout que dice 0

--- src/demojuego/DemoJuego.java
+++ src/demojuego/DemoJuego.java
@@ -5,7 +5,8 @@ public class DemoJuego {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
+        int a = 0;
+        System.out.println(a);
         
         
         
