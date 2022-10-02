021257d6346be18f5f6e63cdcd5406ddd5940921
Alejok0 <ale.ghost32@gmail.com>
2022-10-02 13:34:58 -0500

    Cambio la clase de los Sprites

--- src/demojuego/Sprites.java
+++ src/demojuego/Sprites.java
@@ -1,9 +1,14 @@
 package demojuego;
 
+import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
-public class Sprites extends JFrame{
-    
-    
-    
+public class Sprites extends BufferedImage {
+
+private String ubicacion;
+
+public Sprites(String ubicacion) {
+	this.ubicacion = ubicacion;
+}
+
 }
