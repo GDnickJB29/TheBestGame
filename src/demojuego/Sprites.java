c635c7a767d90c73402a9c8d576de49cb59cd284
Alejok0 <ale.ghost32@gmail.com>
2022-10-02 13:57:02 -0500

    Corrijo errores con la clase Ventana

--- src/demojuego/Sprites.java
+++ src/demojuego/Sprites.java
@@ -1,14 +1,11 @@
 package demojuego;
 
 import java.awt.image.BufferedImage;
-import javax.swing.JFrame;
 
 public class Sprites extends BufferedImage {
 
-private String ubicacion;
+public Sprites() {
 
-public Sprites(String ubicacion) {
-	this.ubicacion = ubicacion;
 }
 
 }
